950b992842b484ed4dccc65908cce6f6ac7ed340
An Nguyen <an.nguyen2@trustingsocial.com>
2022-07-08 17:27:50 +0700

    update: strictly import deps

--- libs/utils-experiments/src/lib/utils-experiments.tsx
+++ libs/utils-experiments/src/lib/utils-experiments.tsx
@@ -1,8 +1,13 @@
+import { Utils } from "@native-nx-ws/utils"
+// cannot import any package, except: @native-nx-ws/utils
+// import { Heading } from "@native-nx-ws/dop-ui";
+
 export const UtilsExperiments = () => {
   return (
     <>
       this is <b>utilsExperiments</b>
       <br />
+      <Utils />
     </>
   )
 }
